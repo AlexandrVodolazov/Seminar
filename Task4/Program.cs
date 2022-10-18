e71f12ffde8255cdd9197ca3adc0c32f30843441
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-18 14:57:43 +0300

    Программа вычисления модуля числа.

---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Программа вычисления модуля числа.
+//2->2
+//-3->3
+//-7->7
+
+Console.Write("Please type the number: ");
+int number = int.Parse(Console.ReadLine());
+
+int mod = number;
+if (number < 0)
+{
+    mod = -number;
+}
+
+Console.WriteLine(mod + "is abs of number");   // так же эту строку можно записать: ($"{mod} is abs of number");
+
+
+
+
