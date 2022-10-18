1af0d014fdf23f00e737afcea24d4dc16f4ab9c5
Alexandr Vodolazov <Vodolazov.09@mail.ru>
2022-10-18 15:15:13 +0300

    Программа, котрая на входе принимает значение, а на выходе показывает обратное значение.

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, котрая на входе принимает значение, а на выходе показывает обратное значение.
+// 1 -> 1
+// 2 -> 0.5
+// 0.25 -> 4
+
+Console.Write("Введие число: ");
+string inputString = Console.ReadLine();
+Double number = Convert.ToDouble(inputString);
+Double ReverNumber = 1 / number;
+Console.Write($"{ReverNumber:F2}");   // Округление, 2 знака после запятой.
+
+
+
